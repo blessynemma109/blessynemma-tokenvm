1482d8d252847777043fc4b97dbbde451c82a2c6
blessynemma109 <eblessing109@gmail.com>
2024-07-23 06:31:32 +0000

    blessing

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "emma"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "blessing"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "bless"
 )
 
 var ID ids.ID
